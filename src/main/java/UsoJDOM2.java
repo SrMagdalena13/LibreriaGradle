5cf5cc501762c5b2a1058e3542a3460eadc35900
Sergio Fernandez Martinez <sfernandezmartinez@danielcastelao.org>
2022-03-15 22:01:09 +0100

    Código Comentado

--- src/main/java/UsoJDOM2.java
+++ src/main/java/UsoJDOM2.java
@@ -11,22 +11,24 @@ import java.util.List;
 public class UsoJDOM2 {
     public static void main(String[] args) {
         try {
-            SAXBuilder builder = new SAXBuilder();
-            File xml = new File("localidad_36057.xml");
-            Document document = builder.build(xml);
+            // Lo primero que haremos es buildear nuestro XML para obtener el elemento raíz
+            SAXBuilder builder = new SAXBuilder(); //Objeto Constructor
+            File xml = new File("localidad_36057.xml"); //Creamos fichero donde se encuenta el XML que queremos parsear
+            Document document = builder.build(xml); //Se construye
             Element root = document.getRootElement();
+            //Cogemos el elemento hijo predicción
+            List<Element> list = root.getChildren("prediccion"); //Lo metemos en una lista
 
-            List<Element> list = root.getChildren("prediccion");
-
+            //Ahora leemos cada uno de los campos además del elemento predicción
             System.out.println("día\tviento\ttemperatura");
 
-            for (int i = 0; i < list.size(); i++) {
+            for (int i = 0; i < list.size(); i++) { //recorremos la lista
 
                 Element prediccion = list.get(i);
 
                 List<Element> valores_prediccion = prediccion.getChildren();
 
-                for (int j = 0; j < valores_prediccion.size(); j++) {
+                for (int j = 0; j < valores_prediccion.size(); j++) { //recorremos la lista
 
                     Element campo = valores_prediccion.get(j);
 
@@ -35,7 +37,7 @@ public class UsoJDOM2 {
                     String temperatura = campo.getChildTextTrim("maxima");
 
 
-                    System.out.println(fecha + "\t\t" + periodo + "\t\t" + temperatura);
+                    System.out.println(fecha + "\t\t" + periodo + "\t\t" + temperatura); //Sacamos por pantalla el resultado
 
                 }
             }
--- src/main/java/UsoJSOUP.java
+++ src/main/java/UsoJSOUP.java
@@ -9,11 +9,14 @@ import static java.rmi.server.LogStream.log;
 
 public class UsoJSOUP {
     public static void main(String[] args) throws IOException {
+        //Creamos un documento con el Jsoup y le añadimos el enlace de la pagina que queremos parsear
         Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/9828ef4989632378cb01f05c18a0ca3a47781f777d705291ec60422ef13d871c").get();
         log(doc.title());
+        //Introducimos el elemento que queremos encontar y sacar
         Elements newsHeadlines1 = doc.select(".CurrentConditions--location--kyTeL");
         Elements newsHeadlines2 = doc.select(".CurrentConditions--tempValue--3a50n");
         Elements newsHeadlines3 = doc.select(".CurrentConditions--phraseValue--2Z18W");
+        //Recorremos cada uno de los elementos y sacamos por pantalla lo que buscabamos
         for (Element headline : newsHeadlines1) {
             System.out.println(headline.html());
         }
